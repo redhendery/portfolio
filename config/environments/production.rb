f984432c8ff2f9ae7facd88ec9a1a8468c727911
redhendery <redhendery@gmail.com>
2020-05-25 12:02:16 -0700

    complete portfolio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,22 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "portfolio_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp-relay.sendinblue.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDINBLUE_USERNAME'],
+    :password       => ENV['SENDINBLUE_PASSWORD'],
+    :domain         => 'blogus-latinus.herokuapp.com',
+    :enable_starttls_auto => true
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
