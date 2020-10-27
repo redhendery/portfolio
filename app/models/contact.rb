b92ecca43d588d0078e4ac277082ed31627619e8
redhendery <redhendery@gmail.com>
2020-10-27 15:20:52 -0700

    add field to prevent spam email on contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,14 +2,13 @@ class Contact < MailForm::Base
   attribute :name
   attribute :email
   attribute :message
+  attribute :ham_sandwich
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
   validates :message, presence: true, length: { minimum: 10 }
 
-  # Declare the e-mail headers. It accepts anything the mail method
-  # in ActionMailer accepts.
   def headers
     {
       subject: 'Contact from completed from your Portfolio',
